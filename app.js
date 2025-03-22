bbd54b22d327cac68c3e55f012a34c1243757a67
shivam khatri <shivamkhatri98960@gmail.com>
2025-03-22 10:06:19 +0530

    jghjhgfdg

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const indexrouter=require('./Router/index')
 const express=require('express')
 const app = express()
+const cors = require('cors');
 app.use(express.json())
-
+app.use(cors());
 
 const port = 3000;
 
